3a79cdeea549f6122d64b1e7e2dc9968c74ffc3a
zamirul <javshazib@gmail.com>
2026-07-03 22:29:11 +0600

    updated catgroy page

--- website/src/components/filters/index.ts
+++ website/src/components/filters/index.ts
@@ -2,4 +2,3 @@
 export { FilterSection } from "./FilterSection";
 export { PriceFilter } from "./PriceFilter";
 export { DiscountFilter } from "./DiscountFilter";
-export { CategoryFilter } from "./CategoryFilter";
